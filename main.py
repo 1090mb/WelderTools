bf28a9fb8e18fec16e7caaab43b7111e3b4dc08f
1090mb <1090mb@protonmail.com>
2026-02-17 04:56:59 -0500

    Fix: Add iOS build workflow and update main.py for iOS compatibility

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.spinner import Spinner
-from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
-from kivy.uix.gridlayout import GridLayout
-from kivy.core.window import Window
+from kivy.uix.spinner import Spinner
+from kivy.uix.textinput import TextInput
+
 from welder_tools import WeldingExpert
 
 
